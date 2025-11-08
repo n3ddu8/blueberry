aba9cb49098f645b6f64663a5d83a850a36199d2
n3ddu8 <13167147+n3ddu8@users.noreply.github.com>
2025-11-08 12:45:54 +0000

    feat: added cockpit packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+	tmux \
+	cockpit-system \
+	cockpit-ostree \
+	cockpit-podman
 
 # Use a COPR Example:
 dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
